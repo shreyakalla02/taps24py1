57b64ac82298316f7c63afa88f977888e63b7218
shreyakalla02 <150719217+shreyakalla02@users.noreply.github.com>
2025-01-30 19:07:36 +0000

    begin

---
+++ main.py
@@ -0,0 +1,18 @@
+# Python 1
+# Basic Data Types
+
+h = "hello"
+w = "world"
+
+print(h,w)
+print(h+w)
+
+n1 = 5
+
+# How to concat str with number 
+# Typecasting 
+# str() --> cast to str,
+# int() --> cast to int, 
+# float() --> cast to float 
+
+print(h+str(n1))
