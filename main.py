ca258ae66ef8243361b9a0d115e6cf4b6072a6c0
shreyakalla02 <150719217+shreyakalla02@users.noreply.github.com>
2025-01-30 19:29:33 +0000

    end of class

--- main.py
+++ main.py
@@ -11,8 +11,35 @@ n1 = 5
 
 # How to concat str with number 
 # Typecasting 
-# str() --> cast to str,
-# int() --> cast to int, 
-# float() --> cast to float 
+multilinestring = """
+        str() --> cast to str,
+        int() --> cast to int, 
+        float() --> cast to float 
+    """
 
 print(h+str(n1))
+
+# Formatted strings 
+n1 = 5
+n2 = 3
+res = n1 + n2
+out = f"{n1}+{n2}={res}"
+print(out)
+
+# Logical operators 
+# Code block 
+if n1<n2: 
+    print("Lesser")
+    print("<"*6)
+elif n1 == n2:
+    print("Equal")
+    print('====')
+else:
+    print("Greater")
+    print(">"*len("Greater"))
+
+# blocks in python use indentation 
+# used in if statements / function def 
+
+f = 17.4
+print(type(f))
